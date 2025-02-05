6b311e590bed4ede8bdde228d1042d392d6bf1ac
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-02-05 15:51:38 +0000

    ajout d'une transition fluide entre les sections

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+
+// Sélectionner tous les liens de navigation
+const links = document.querySelectorAll('a[href^="#"]');
+
+// Ajouter un événement 'click' sur chaque lien
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    const targetId = link.getAttribute('href').substring(1); // Obtenir l'ID de la section cible
+    const targetElement = document.getElementById(targetId); // Sélectionner l'élément cible
+
+    // Défilement fluide vers l'élément cible
+    targetElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start' // Optionnel, pour définir la position du défilement
+    });
+  });
+});
+
+
+
+
 // Sélection des éléments HTML
 let btn = document.getElementById("parcoursBtn");
 let texte = document.getElementById("parcoursTexte");
